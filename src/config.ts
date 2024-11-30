776564443bf0c9485c03d5ed00cf9974ac665191
nathy <njmackinlay@gmail.com>
2024-11-29 21:34:20 -0700

    Initial setup.

---
+++ src/config.ts
@@ -0,0 +1,13 @@
+type Config = {
+  author: string;
+  title: string;
+  description: string;
+  lang: string;
+};
+
+export default {
+  author: "Nathy @ Old Growth Games",
+  title: "Old Growth Games",
+  description: "Old Growth Games overview",
+  lang: "en",
+} satisfies Config;
